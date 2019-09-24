71c94754f247533ee5a6df1225ec13b3a7d1fa4b
meninomiel <meninomiel@hotmail.com>
2019-09-24 17:06:42 -0300

    pequeno ajuste no cadastro de jogadores

--- server/app/routes/player.js
+++ server/app/routes/player.js
@@ -11,25 +11,23 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next){
-  console.log(req.body);
+  //console.log(req.body);
+
   if (players.length == 0){
     players.push(req.body);
   } else {
-    let contains = false
-    players.map(player => {
-      if (player.nome == req.body.nome){
-        Object.assign(player, req.body);
-        contains = true;
-        console.log(`Jogador ${req.body.nome} atualizado `);
-      } 
-    });
-    if (contains == false) {
+    let containPlayer = players.find(player => player.nome === req.body.nome);
+
+    if (containPlayer){
+      Object.assign(containPlayer, req.body);
+      console.log(`Jogador ${req.body.nome} atualizado `);
+    } else {
       players.push(req.body);
       console.log(`Jogador ${req.body.nome} inserido `);
     }
   }  
   
-  console.log(players);
+  //console.log(players);
   res.status(200).send(players);
   
 });
